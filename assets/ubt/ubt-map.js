9268adc58912ff8974cf99194cc5e658201ed395
Tufva Linde <tufva@tufvalinde.com>
2025-12-03 23:43:42 -0600

    test

--- assets/ubt/ubt-map.js
+++ assets/ubt/ubt-map.js
@@ -140,7 +140,7 @@ function drawEverything() {
         html: `<div style="font-size:30px;color:${pinColor}">📍</div>`,
         className: '',
         iconSize: [24,24],
-        iconAnchor: [5,5]
+        iconAnchor: [5,-15]
       });
     }
 
